6c2ee80a33a7040520758acb5977cef9fdc4f3ee
Thomas Lissens <th.lissens@gmail.com>
2013-01-28 01:07:47 +0100

    Je peux enfin recommiter!

--- web/js/code.js
+++ web/js/code.js
@@ -1,113 +1,52 @@
+
 $(function(){
-	//$('a[rel*=external]').attr('target', '_new');
-	
-	var $images = $(".choice"); 
-	
-	$images.first().children('.choice').children('label').attr("checked", "checked");
-	
-	$images.not(':first').hide();
-	//$(".selectionner").hide();
 
-	
-	$("#next").on("click", switchNext);
-	$("#previous").on("click", switchPrevious);
+    // Définitions globales
+    var $images = $(".choice");
 
+    //Attr checked sur la première img
+ 	 $images.first().children(".selectionner").attr("checked","checked");
 
-	function switchPrevious(e){
-		e.preventDefault();
-		var $nextImage = $images.filter(':visible').prev(".choice");
-		
-		if( $nextImage.size() == 0 )
-			$nextImage = $images.last();
-		
-		$images.filter(':visible').fadeOut('fast', function(){
-			$nextImage.fadeIn('fast');
-		});
-		
-		$nextImage.children('.choice').children('.selectionner').attr("checked", true);
-		
-	}
-	
-	function switchNext(e)
-	{
-		e.preventDefault();
-		var $nextImage = $images.filter(':visible').next(".choice");
-		
-		if( $nextImage.size() == 0 )
-			$nextImage = $images.first();
-		
-		$images.filter(':visible').fadeOut('fast', function(){
-			$nextImage.fadeIn('fast');
-		});
-		
-		$nextImage.children('.choice').children('.selectionner').attr("checked", true);
-	}
-	
-});
+    //Cache tout sauf la première img
+    $images.not(':first').hide();
 
+    //cache les boutons radio
+    $(".selectionner").hide();
 
-$(".delete").on("click", function(event){
-	var lien = $(this).attr("href");
-	var lienId = $(this).attr("rel");
-	event.preventDefault();
-	$.ajax({
-		url: lien,
-		success : function(){ 
-				$('#lien_'+lienId).fadeOut('fast');
-		}
-	});
-	
-	return false;
-});
+    //Au clique sur button next, lance function switchNext
+    $("#next").on("click", switchNext);
 
+    //Au clique sur button previous, lance function switchPrevious
+    $("#previous").on("click", switchPrevious);
 
-/*
-$("label[type=submit]").on("click", function(event){
-	event.preventDefault();
-	alert($("label[name=titre]").val());
+    //Img suivante
+    function switchNext(e)
+    {
+        e.preventDefault();
+        var $nextImage = $images.filter(':visible').next(".choice");
 
-	var form_data = {
-	titre : $("label[name=titre]").val(),
-	description : $("label[name=description]").val(),
-	image : 'test'
-	};
-	
-	$.ajax({
-			type: "POST",
-			url: $(this).parent().attr("action"),
-			async : false,
-			data: form_data,
-			success : function(){ 
-					alert("ok");
-			}
-		});
+        if( $nextImage.size() == 0 )
+            $nextImage = $images.first();
+        $images.filter(':visible').fadeOut('fast', function(){
+            $nextImage.fadeIn('fast');
+        });
 
-	
-	return false;
+        //ajoute attribut checked avec valeur checked au bouton radio
+        $nextImage.children(".selectionner").attr("checked", "checked");
+    }
 
-});
-*/
+    //Img précédente
+    function switchPrevious(e){
+            e.preventDefault();
+            var $nextImage = $images.filter(':visible').prev(".choice");
 
+            if( $nextImage.size() == 0 )
+                $nextImage = $images.last();
+            $images.filter(':visible').fadeOut('fast', function(){
+                $nextImage.fadeIn('fast');
+            });
 
-/*
-$("#connexionForm").submit( function() {	// à la soumission du formulaire						 
-	$.ajax({ // fonction permettant de faire de l'ajax
-	   type: "POST", // methode de transmission des données au fichier php
-	   url: "login.php", // url du fichier php
-	   data: "login="+$("#login").val()+"&pass="+$("#pass").val(), // données à transmettre
-	   success: function(msg){ // si l'appel a bien fonctionné
-			if(msg==1) // si la connexion en php a fonctionnée
-			{
-				$("div#connexion").html("<span id=\"confirmMsg\">Vous &ecirc;tes maintenant connect&eacute;.</span>");
-				// on désactive l'affichage du formulaire et on affiche un message de bienvenue à la place
-			}
-			else // si la connexion en php n'a pas fonctionnée
-			{
-				$("span#erreur").html("<img src=\"bomb.png\" style=\"float:left;\" />&nbsp;Erreur lors de la connexion, veuillez v&eacute;rifier votre login et votre mot de passe.");
-				// on affiche un message d'erreur dans le span prévu à cet effet
-			}
-	   }
-	});
-	return false; // permet de rester sur la même page à la soumission du formulaire
-});
-*/
+            //ajoute attribut checked avec valeur checked au bouton radio
+ $nextImage.children(".selectionner").attr("checked", "checked");
+    }
+}); 
